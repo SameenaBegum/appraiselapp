56bd569000057ca1b68f9bb2e31aa152a96e8479
SameenaBegum <sameenabegum.s@skeintech.com>
2023-03-01 10:06:41 +0530

    cors added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 //const morgan = require('morgan');
 var indexRouter = require('./routes/index.routes');
-//const cors = require('cors');
+const cors = require('cors');
 //var cookieParser = require('cookie-parser');
 //const passport = require('passport')
 
@@ -9,7 +9,7 @@ const app = express();
 //app.use(express.json({limit: '50mb'}));
 //app.use(morgan('dev'));
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 //app.use(cookieParser());
 //const { ExtractJwt, Strategy } = require('passport-jwt');
 
